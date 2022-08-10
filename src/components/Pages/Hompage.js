b2c43fe2d59174bdede7f48b86db6555acaa1d21
Jackson <98294869+Daiz3002@users.noreply.github.com>
2022-08-10 21:19:49 +1200

    Nav partial done issues with logo, Homepage mostly done beginning db

--- src/components/Pages/Hompage.js
+++ src/components/Pages/Hompage.js
@@ -1,29 +1,29 @@
 import React, {useEffect, useState} from "react";
 import Travels from "./Travels";
+import HeroSection from "../HeroSection.js"
 
 function Homepage() {
-    return <div>
-        Homepage
-    </div>;
+    const [travels, setTravels] = useState([])
+    useEffect(()=>{
+        // fetch("https://travelbriefing.org/Netherlands?format=json")
+        fetch("http://localhost:4000/Travels")
+        .then((r)=>r.json())
+        .then((data)=>{
+            console.log(data);
 
-    function getData()
-    {
-        const [travels, setTravels] = useState([])
-        useEffect(()=>{
-            // fetch("https://travelbriefing.org/Netherlands?format=json")
-            fetch("http://localhost:4000/Travels")
-            .then((r)=>r.json())
-            .then((data)=>{
-                console.log(data);
-
-                setTravels(data);
-            })
-        },[])
-        return <ul>
-            {travels.map(item=>{return <Travels key={item.id} result={item}/>})}
-        </ul>
-    }
+            setTravels(data);
+        })
+    },[])
+    return <div className="Container">
+        <div className="Row">
+            <ul>
+                {travels.map(item=>{return <Travels key={item.id} result={item}/>})}
+            </ul>
+        </div>
+        <HeroSection/>
+    </div>
 }
+
 
 
 export default Homepage;
